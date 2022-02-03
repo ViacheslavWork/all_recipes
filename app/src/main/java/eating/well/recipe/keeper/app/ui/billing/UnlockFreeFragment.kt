9156e45c7e0c3f78a1a9f261f670ed517e5b6ddb
Viacheslav <demon55591@yandex.ru>
2022-02-03 13:27:13 +0300

    unlock fragment created

---
+++ app/src/main/java/eating/well/recipe/keeper/app/ui/billing/UnlockFreeFragment.kt
@@ -0,0 +1,19 @@
+package eating.well.recipe.keeper.app.ui.billing
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import eating.well.recipe.keeper.app.R
+
+
+class UnlockFreeFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_unlock_free, container, false)
+    }
+}
